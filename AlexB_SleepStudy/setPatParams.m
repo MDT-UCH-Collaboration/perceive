8fb115bb1d1cc1e31585a1acf3ce04a54d8b2c1f
johnarthurthompson <john.a.thompson@cuanschutz.edu>
2022-03-10 09:59:29 -0700

    update alex sleep files

---
+++ AlexB_SleepStudy/setPatParams.m
@@ -0,0 +1,19 @@
+patParams = struct;
+
+%% 1
+patParms.P1.hemi = 'R';
+patParms.P1.ID = 1;
+patParms.P1.OverSat = true;
+patParms.P1.json = 'Report_Json_Session_Report_20210521T115931[1].json';
+
+%% 2
+patParms.P2.hemi = 'L';
+patParms.P2.ID = 2;
+patParms.P2.OverSat = false;
+patParms.P2.json = 'Report_Json_Session_Report_20210521T105436[1].json';
+
+%% 3
+patParms.P2.hemi = 'L';
+patParms.P2.ID = 3;
+patParms.P2.OverSat = false;
+patParms.P2.json = 'Report_Json_Session_Report_20210521T105436[1].json';
