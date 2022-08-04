a02f8ff926520a819c81c3247675b5086d9011a3
John Thompson <john.a.thompson@cuanschutz.edu>
2022-08-04 16:38:48 -0600

    update updrs plot

--- postAnalysis/updrsPlot.m
+++ postAnalysis/updrsPlot.m
@@ -1,12 +1,52 @@
-cd('D:\Dropbox\MDT_PhaseI_Manuscripts')
+name = getenv('COMPUTERNAME');
+if matches(name,'DESKTOP-I5CPDO7')
+    cd('C:\Users\Admin\Dropbox\MDT_PhaseI_Manuscripts\Figures\Data')
+else
+    cd('D:\Dropbox\MDT_PhaseI_Manuscripts')
+end
 
 upTab = readtable('updrsSum.xlsx');
 
 offD = matches(upTab.cond,'OFF');
 entD = matches(upTab.cond,'Entry');
-expD = matches(upTab.cond,'Exp');
-expDa = matches(upTab.cond,'Exp') & matches(upTab.ExpType, 'maxb');
-expDb = matches(upTab.cond,'Exp') & ~matches(upTab.ExpType, 'maxb');
+mbp30 = matches(upTab.cond,'MBP_30');
+mbp60 = matches(upTab.cond,'MBP_60');
+mbp90 = matches(upTab.cond,'MBP_90');
+
+coloRS = [0.1490    0.2745    0.3255;
+          0.1647    0.6157    0.5608; 
+          0.9137    0.7686    0.4157;
+          0.9569    0.6353    0.3804;
+          0.9059    0.4353    0.3176];
+
+% scatterPall
+
+% scatter([1 3 5; 2 4 6],[10 25 45; 20 40 60])
+
+xAXis = repmat(1:5,sum(offD),1);
+
+yAXis = [upTab.TOTAL(offD),upTab.TOTAL(entD),upTab.TOTAL(mbp30),...
+         upTab.TOTAL(mbp60),upTab.TOTAL(mbp90)];
+
+s1 = scatter(xAXis,yAXis);
+
+for ssi = 1:5
+    s1(ssi).MarkerEdgeColor = coloRS(ssi,:);
+    s1(ssi).MarkerFaceAlpha = 0.3;
+    s1(ssi).MarkerFaceColor = coloRS(ssi,:);
+    s1(ssi).SizeData = 60;
+end
+
+
+xlim([0.5 5.5]);
+xticks(1:5)
+xticklabels({'OFF','Entry','MBP30','MBP60','MBP90'})
+
+% LINE or STAR for mean
+
+
+
+
 
 line([zeros(1,sum(offD)) ; ones(1,sum(offD))] ,...
     [transpose(upTab.TOTAL(offD)) ; transpose(upTab.TOTAL(entD))],...
