74e8467e290dffc75a51ce12031144a98f0cdd72
John A. Thompson <john.a.thompson@cuanschutz.edu>
2023-04-05 08:36:21 -0600

    update TEED

--- AlexB_SleepStudy/revision_TEED.m
+++ AlexB_SleepStudy/revision_TEED.m
@@ -1,33 +1,48 @@
-monoTab = js.Impedance.Hemisphere.SessionImpedance.Monopolar;
-
-% Electrode2 = get number that was used for stimulation
-% ohn, could we double check the JSON file for this to make sure we're right? It's subject #6
-% PW, Frequency, amplitude
-
-finGroup = js.Groups.Final;
-settINGS = finGroup.ProgramSettings;
-ratEE = settINGS.RateInHertz;
-sensCCg = settINGS.SensingChannel;
-pulseWt = sensCCg.PulseWidthInMicroSecond;
-
-% ResultValue
-imped = monoTab(contains({monoTab.Electrode2},'ElectrodeDef.FourElectrodes_0'),:);
-impedU = imped.ResultValue;
-
-averageAmp = 2.1;
-
-teed = averageAmp^2 * impedU * ratEE * pulseWt;
-% 76776336 = 2.1^2 * 1488 * 90 * 130
+% monoTab = js.Impedance.Hemisphere.SessionImpedance.Monopolar;
+% 
+% % Electrode2 = get number that was used for stimulation
+% % ohn, could we double check the JSON file for this to make sure we're right? It's subject #6
+% % PW, Frequency, amplitude
+% 
+% finGroup = js.Groups.Final;
+% settINGS = finGroup.ProgramSettings;
+% ratEE = settINGS.RateInHertz;
+% sensCCg = settINGS.SensingChannel;
+% pulseWt = sensCCg.PulseWidthInMicroSecond;
+% 
+% % ResultValue
+% imped = monoTab(contains({monoTab.Electrode2},'ElectrodeDef.FourElectrodes_0'),:);
+% impedU = imped.ResultValue;
+% 
+% averageAmp = 2.1;
+% 
+% teed = averageAmp^2 * impedU * ratEE * pulseWt;
+% % 76776336 = 2.1^2 * 1488 * 90 * 130
 
 
 %%
 
-cd('E:\Dropbox\Publications_Meta\InProgress\ABaumgartner_Percept2020')
-load('PatParms.mat','patParams')
+computerName = getenv("COMPUTERNAME");
 
-mainDIR = 'E:\Dropbox\Publications_Meta\InProgress\ABaumgartner_Percept2020';
+switch computerName
+    case 'DESKTOP-EGFQKAI'
+
+        cd('C:\Users\johna\Dropbox\Publications_Meta\InProgress\ABaumgartner_Percept2020')
+        load('PatParms.mat','patParams')
+        mainDIR = 'C:\Users\johna\Dropbox\Publications_Meta\InProgress\ABaumgartner_Percept2020';
+
+    otherwise
+
+        cd('E:\Dropbox\Publications_Meta\InProgress\ABaumgartner_Percept2020')
+        load('PatParms.mat','patParams')
+        mainDIR = 'E:\Dropbox\Publications_Meta\InProgress\ABaumgartner_Percept2020';
+
+
+end
 userDIRs = [mainDIR,'\Data\SPPD'];
 userDIRe = '\JSON_LFP';
+
+
 
 stimmA = [3 , 3.2 , 2.0 , 2.6 , 3.1 , 2.0 , 4.4, 3.8, 3.4, 4.6, 6, 2.6, 2.0, 2.5, 1.0, 1.8, 1.8, 1.5 ];
 elecID = [1 1 1 2 2 1 2 1 1 2 2 2 1 1 1 2 2 2];
